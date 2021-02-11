8e9e682db80dd46df3120842fc13dc0024e174fe
Ross Ferreira <ross.p.ferreira@gmail.com>
2021-02-11 22:11:27 +0000

    finished lecture 338

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'tachyons';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+// react portal allows you to transfer props between react Apps
